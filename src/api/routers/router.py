7f0a7acf72abb7c7b8297804570db878623b0f73
Gr <gavrilov_ga@bk.ru>
2025-07-09 01:10:52 +0300

    added couple of vacancy page and question generation

--- src/api/routers/router.py
+++ src/api/routers/router.py
@@ -2,6 +2,11 @@ from fastapi import APIRouter, Depends, HTTPException, Response
 from authx import AuthX, AuthXConfig
 from pydantic import BaseModel
 
+
+openai_api_key = 'sk-SwDj282n2Anwwu96iaisTDxPUf1n15hG'
+
+
+
 router = APIRouter()
 config = AuthXConfig()
 config.JWT_SECRET_KEY = "your_secret_key"
@@ -32,3 +37,69 @@ async def login(creds: UserLoginSchema, response: Response):
 @router.get("/setting", dependencies=[Depends(security.access_token_required)])
 async def setting():
     return {"message": "You are authenticated as HR and can set interview parameters."}
+
+
+@router.get("/vacancies")
+async def vacancy():
+    '''тут будет отображение списка вакансий'''
+    return 'ВАКАНСИЙ НЕТ'
+
+
+class VacancyData(BaseModel):
+    title: str
+    tags: str
+    
+
+
+@router.post('/vacancies')
+async def vacancy(vac_data: VacancyData):
+    '''тут будет создание вакансии'''
+    return {
+        'title': vac_data.title,
+        'tags': vac_data.tags
+    }
+    
+    
+@router.get('/vacancies/{vacancy_id}')
+async def vacancy_detail(vacancy_id: int):
+    '''тут будет отображение вакансии по id'''
+    return {
+        'vacancy_id': vacancy_id,
+        'title': 'Example Vacancy',
+        'tags': 'example, vacancy'
+    }
+
+
+def generate_questions_ai(title: str, tags: str):
+    import openai
+    prompt = (f"Ты — опытный hr-специалист. Сейчас ты составляешь вопросы для собеседования на вакансию '{title}'. "
+        f"Кандидат должен обладать навыками: {tags}. Сгенерируй ровно 15 вопросов. "
+        "Вопросы должны быть релевантны вакансии и направлены на оценку как профессиональных навыков, так и soft skills. Выведи просто список из вопросов без пояснений или дополнительных комментариев."
+        )
+    
+    client = openai.OpenAI(
+    api_key=openai_api_key,
+    base_url="https://api.proxyapi.ru/openai/v1"
+    )
+    messages=[
+                {"role": "user", "content": prompt}
+    ]
+    response = client.chat.completions.create(
+        model="gpt-4o",
+        messages=messages,
+        max_tokens=1000,
+        temperature=0.7
+    )
+    
+    ans = response.choices[0].message.content
+    return {
+        'title': title,
+        'tags': tags,
+        'questions': ans
+    }
+
+
+@router.post('/vacancies/questions')
+async def add_questions(vac_data: VacancyData):
+    return generate_questions_ai(vac_data.title, vac_data.tags)
+
